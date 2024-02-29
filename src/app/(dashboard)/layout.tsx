8abfa5e99b3ef9315c6efa5275a5e89fc683db35
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2024-02-29 00:47:48 -0300

    config shadcn-ui and theme

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
+      <main className='flex w-full flex-grow'>{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 
+import { ThemeProvider } from '@/components/providers/theme-provider';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,7 +21,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
